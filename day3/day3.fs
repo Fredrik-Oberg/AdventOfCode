d3904f2d08c79ded37525aa38c3ea77fba987a5c
Fredrik Öberg <fredrik.oberg@ezy.se>
2016-12-19 20:25:10 +0100

    Day3 puzzle A finished Update input to the correct one

--- day3/day3.fs
+++ day3/day3.fs
@@ -1,27 +1,24 @@
 open System.Collections.Generic
 open System
 let squaresWithThreeSides(input:IEnumerable<string>) =
-//    let isTriangle values =
-//        match values with
-//        | (a)
-//        a + b < c
+    let isTriangle(a:int, b:int, c:int) =
+        if a + b > c && a + c > b && b + c > a  then true
+        else false
 
     let parseToInt(x:string[]) =
         [Int32.Parse(x.[0]); Int32.Parse(x.[1]); Int32.Parse(x.[2])]
 
     let res = input
-            |> Seq.map (fun x -> x.Trim() )
-            |> Seq.map (fun x -> x.Split ([|' '|], StringSplitOptions.RemoveEmptyEntries))
+            |> Seq.map (fun x -> x.Trim())
+            |> Seq.map (fun x ->  x.Split ([|' '|], StringSplitOptions.RemoveEmptyEntries))
             |> Seq.map (fun x -> parseToInt x)
-//            |> Seq.map (fun x -> isTriangle())
+            |> Seq.map (fun x -> isTriangle(x.[0], x.[1],x.[2]))
 
     res
 
-let path = __SOURCE_DIRECTORY__ + "/input.txt"
+let path = __SOURCE_DIRECTORY__ + "/inputs.txt"
 let input = System.IO.File.ReadLines path
 
 let result = squaresWithThreeSides input
-//let HowManySatisfy pred = Seq.filter pred >> Seq.length
-//result |> HowManySatisfy (fun x -> x = true)
 
-result |> Seq.iter (fun x -> printfn "%A" x )
+result |> Seq.filter (fun x -> x = true) |> Seq.length
