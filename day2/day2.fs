2c11558a0b8157543b2f3d606feb8b97129d70ce
Fredrik Öberg <fredrik.oberg@ezy.se>
2016-12-21 14:29:51 +0100

    Day2 puzzle A done

--- day2/day2.fs
+++ day2/day2.fs
@@ -4,11 +4,9 @@ let bathroomSecurity(input:System.Collections.Generic.IEnumerable<string>) =
     let directions = input  |> Seq.map (fun line -> line.ToCharArray() |> Array.toList) |> Seq.toList
 
     let move (direction:char, currentVal:int) =
-        printfn "currentVal %A" currentVal
-        printfn "direction %A" direction
         match direction with
-        | 'U' -> if currentVal < 3 then currentVal else currentVal - 3
-        | 'D' -> if currentVal > 6 then currentVal else currentVal + 3
+        | 'U' -> if currentVal < 4 then currentVal else currentVal - 3
+        | 'D' -> if currentVal > 7 then currentVal else currentVal + 3
         | 'L'
             -> match currentVal with
                 | 1 | 4 | 7 -> currentVal
@@ -20,30 +18,27 @@ let bathroomSecurity(input:System.Collections.Generic.IEnumerable<string>) =
         | _ -> currentVal
 
 
-    let rec getCode listOfDirections resultList:List<int> =
+    let rec getCode listOfDirections (resultList:List<int>) =
         match listOfDirections with
         | [] ->
-            printfn "finalVal %A" resultList
-            resultList
+            let last = resultList |> Seq.last
+            last
         | hd::tl ->
 
             let lastVal = resultList |> Seq.last
-//            printfn "lastVal %A" lastVal
             let newVal = move(hd, lastVal)
-            printfn "newVal %A" newVal
             resultList.Add(newVal)
             let m = resultList |> Seq.toList
             getCode tl resultList
 
     let initList = new List<int>()
-    initList.Add(5)
+    initList.Add(8)
 
     let result = directions |> Seq.map(fun dir-> getCode dir initList)
 
     result
 
 let input = System.IO.File.ReadLines @"\dev\adventofcode\day2\input.txt"
-
 let result = bathroomSecurity input
 
 result |> Seq.toList
